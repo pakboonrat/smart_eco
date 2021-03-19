ab82ab8e20bde2e0f9e55d0e2ed17d1f8b38b3a6
pakboonrat <kemuta@hotmail.com>
2021-03-19 15:24:40 +0700

    comit 2

---
+++ login.php
@@ -0,0 +1,24 @@
+<html lang="en" class="pc chrome88 js">
+<head>
+<meta charset="UTF-8">
+<title>A Pen by Pak Boonrat</title>
+<link rel = "stylesheet" type = "text/css" href = "css\login.css" >
+
+</head>
+<body>
+<form class="login-form">
+  <p class="login-text">
+    <span class="fa-stack fa-lg">
+      <i class="fa fa-circle fa-stack-2x"></i>
+      <i class="fa fa-lock fa-stack-1x"></i>
+    </span>
+  </p>
+  <input type="username" class="login-username" autofocus="true" required="true" placeholder="USERNAME" />
+  <input type="password" class="login-password" required="true" placeholder="PASSWORD" />
+  <input type="submit" name="Login" value="Login" class="login-submit" />
+</form>
+<a href="#" class="login-forgot-pass">forgot password?</a>
+<div class="underlay-photo"></div>
+<div class="underlay-black"></div> 
+</body>
+</html>
