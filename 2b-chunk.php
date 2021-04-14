1316c56d3535c193b04d4e3133413559e61b6022
pakboonrat <kemuta@hotmail.com>
2021-04-14 14:15:27 +0700

    14april

---
+++ 2b-chunk.php
@@ -0,0 +1,48 @@
+<?php
+// FORMULATE Standart server response
+function verbose($ok=1,$info=""){
+    if($ok==0){ http_response_code(400);}
+    die(json_encode([
+        "ok" => $ok,
+        "info" => $info
+    ]));
+}
+
+// INVALID UPLOAD
+if(empty($_FILES) || $_FILES['file']['error']){
+    verbose(0, "Failed to move uploaded file");
+}
+
+// UPLOAD DESTINATION
+$filePath = __DIR__ . DIRECTORY_SEPARATOR . "uploads";
+if(!file_exists($filePath)){
+    if(!mkdir($filePath, 0777, true)){
+        verbose(0, "Failed to create ".$filePath);
+    }
+}
+$fileName = isset($_REQUEST['name'])?$_REQUEST['name'] : $_FILES["file"]["name"];
+$filePath = $filePath. DIRECTORY_SEPARATOR . $fileName;
+
+// DEAL WITH CHUNKS
+$chunk = isset($_REQUEST["chunk"])?$_REQUEST["chunk"]:0;
+$chunks = isset($_REQUEST["chunks"])?$_REQUEST["chunks"]:0;
+$out = @fopen("{$filePath}.part",$chunk==0?"wb":"ab");
+if($out){
+    $in = @fopen($_FILES["file"]["tmp_name"],"rb");
+    if($in){
+        while($buff=fread($in, 4096)){ fwrite($out, $buff);}
+    }else{
+        verbose(o,"Failed to open input stream");
+    }
+    @fclose($in);
+    @fclose($out);
+    @unlink($_FILES["file"]["tmp_name"]);
+}else{
+    verbose(0,"Failed to open output stream");
+}
+
+// CHECK FULL FILE UPLOADED
+if(!$chunks || $chunk == $chunks - 1){
+    rename("{$filePath}.part", $filePath);
+}
+verbose(1,"Upload OK");
---
+++ upload4.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <title>Upload Chunk</title>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/plupload/3.1.3/plupload.full.min.js"></script>
+        <script>
+            window.addEventListener("load",function(){
+                var uploader = new plupload.Uploader({
+                    runtimes: "html5,html4",
+                    browse_button: "pickfiles",
+                    url:"2b-chunk.php",
+                    chunk_size:"1mb",
+                    init:{
+                        PostInit: function(){
+                            document.getElementById("filelist").innerHTML = "";
+                        },
+                        FilesAdded: function(up, files){
+                            plupload.each(files, function(file){
+                                document.getElementById("filelist").innerHTML +=`<div id="${file.id}">${file.name} (${plupload.formatSize(file.size)}) - <strong>0%</strong></div>`;
+                            });
+                            uploader.start();
+                        },
+                        UploadProgress: function(up, file){
+                            document.querySelector(`#${file.id} strong`).innerHTML = `${file.percent}%`;
+                        },
+                        Error: function(up, err){
+                            console.log(err);
+
+                        }
+                    }
+                });
+                uploader.init();
+            });
+        </script>
+    </head>
+    <body>
+        <!-- UPLOAD Form -->
+        <div id="container">
+            <span id="pickfiles">[upload Files]</span>
+        </div>
+
+        <!-- Upload File List -->
+        <div id="filelist"></div>
+    </body>
+</html>
