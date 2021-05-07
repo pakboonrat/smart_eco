478d627c95680b46804487a277d69a84ed37f55c
pakboonrat <kemuta@hotmail.com>
2021-05-07 13:19:05 +0700

    comit

--- function.php
+++ function.php
@@ -353,8 +353,6 @@ select level_id,sub_lebel,level_label,set_lebel,type from `level` where set_lebe
                             WHEN  T3.status='consider' THEN 1 
                             WHEN  A3.firstname is not null THEN 1 
                             WHEN  T3.status in ('consider','pass','reject') AND A3.firstname is null THEN 1
-                            WHEN  T3.status in ('consider','pass','reject') AND aprove_list_score.status != 'pass' THEN 2  
-                        
                         ELSE 0 END AS status , A3.firstname as firstname 
 					FROM transaction  T3 
 					LEFT JOIN 
@@ -362,9 +360,16 @@ select level_id,sub_lebel,level_label,set_lebel,type from `level` where set_lebe
   						FROM aprove,user 
   						where aprove.audit_id=user.user_id 
 					) A3 ON T3.t_id=A3.t_id 
-					, list
-					LEFT JOIN aprove_list_score ON aprove_list_score.level_id = list.level_id
-                 	WHERE T3.list_id = list.list_id
+                    
+					UNION 
+                    SELECT DISTINCT A4.user_id as user_id ,
+                    CASE  
+                            WHEN  A4.status='pass' THEN 2
+                        
+                        ELSE 0 END AS status , U4.firstname as firstname
+                    FROM aprove_list_score A4 ,user U4 
+					WHERE
+                    A4.audit_id = U4.user_id
                  	
                  
                     UNION
@@ -373,21 +378,13 @@ select level_id,sub_lebel,level_label,set_lebel,type from `level` where set_lebe
                             WHEN  UADD3.status='consider' THEN 1 
                             WHEN  UA3.firstname is not null THEN 1 
                             WHEN  UADD3.status in ('consider','pass','reject') AND UA3.firstname is null THEN 1
-                            WHEN  UADD3.status in ('consider','pass','reject') AND aprove_list_score.status != 'pass' THEN 2  
-                         
-                        
                         ELSE 0 END AS status ,  UA3.firstname as firstname 
 					FROM user_add  UADD3 
 					LEFT JOIN 
 					( 	select DISTINCT aprove_user_add.add_id,user.firstname 
   						FROM aprove_user_add,user 
   						where aprove_user_add.audit_id=user.user_id 
-					) UA3 ON UADD3.add_id=UA3.add_id 
-					, list
-					LEFT JOIN aprove_list_score ON aprove_list_score.level_id = list.level_id 
-                 			AND aprove_list_score.score_id = list.score_id
-					WHERE UADD3.level_id = list.level_id 
-                 			AND UADD3.score_id = list.score_id
+					) UA3 ON UADD3.add_id=UA3.add_id
                  
                 ) T1 ON U1.user_id=T1.user_id
                 WHERE U1.user_type = 'USER'
